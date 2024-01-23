c5a29a8d9e437273f2ab4742ccca1b3ad830b1f4
David Farkas <davidfarkas@192.168.1.15>
2024-01-23 20:31:20 +0100

    Added login test

---
+++ e2e/fixtures/users.ts
@@ -0,0 +1,4 @@
+export const STANDARD_USER = {
+    username:  "standard_user",
+    password: "secret_sauce"
+}
---
+++ e2e/tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/login-page.";
+import { STANDARD_USER } from "../fixtures/users";
+const urlAfterlogin = "https://www.saucedemo.com/v1/inventory.html"
+
+test("Open and verify login page", async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goToPage();
+    await expect(page).toHaveTitle(/Swag Labs/);
+    await loginPage.login(STANDARD_USER.username, STANDARD_USER.password);
+    await expect(page).toHaveURL(urlAfterlogin);
+    
+});
