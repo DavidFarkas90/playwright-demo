da303041aa98d3a3598a7207bbc02e7616876337
David Farkas <davidfarkas90@gmail.com>
2024-02-21 20:45:52 +0100

    Added parametirized API test

--- e2e/tests/api-users.spec.ts
+++ e2e/tests/api-users.spec.ts
@@ -4,18 +4,46 @@ import { test, expect } from '@playwright/test';
 const URL = "https://jsonplaceholder.typicode.com";
 const TITLE = "Testing posts";
 
-test('GET users', async ({ request }) => {
+const USERS = [
+    {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret"
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv"
+    },
+    {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nathan@yesenia.net"
+    },
+    {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "Julianne.OConner@kory.org",
+    }];
+for (const USER of USERS) {
+    test(`GET user with id: ${USER.id}`, async ({ request }) => {
 
-    const response = await request.get(`${URL}/users/1`);
-    expect(response).toBeTruthy();
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
-    const responseBody = await response.json();
-    expect(responseBody).toHaveProperty("name", "Leanne Graham");
-    expect(responseBody.username).toEqual("Bret");
+        const response = await request.get(`${URL}/users/${USER.id}`);
+        expect(response).toBeTruthy();
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty("name", `${USER.name}`);
+        expect(responseBody.username).toEqual(`${USER.username}`);
 
 
-});
+    });
+}
+
+
 
 test('Create new post', async ({ request }) => {
     const newUserResponse = await request.post(`${URL}/posts`, {
