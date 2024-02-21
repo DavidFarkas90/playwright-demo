4535bb3a60a5b908d4d861b40c62087ab8747ee0
David Farkas <davidfarkas90@gmail.com>
2024-02-21 20:26:13 +0100

    Added POST test

--- e2e/tests/api-users.spec.ts
+++ e2e/tests/api-users.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
 
+
+const URL = "https://jsonplaceholder.typicode.com";
+const TITLE = "Testing posts";
+
 test('GET users', async ({ request }) => {
 
-    const response = await request.get("https://jsonplaceholder.typicode.com/users/1");
+    const response = await request.get(`${URL}/users/1`);
     expect(response).toBeTruthy();
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
@@ -10,5 +14,20 @@ test('GET users', async ({ request }) => {
     expect(responseBody).toHaveProperty("name", "Leanne Graham");
     expect(responseBody.username).toEqual("Bret");
 
+
+});
+
+test('Create new post', async ({ request }) => {
+    const newUserResponse = await request.post(`${URL}/posts`, {
+        data: {
+            title: TITLE,
+            body: 'this is my body',
+            userId: 1
+        }
+    });
+    expect(newUserResponse.ok()).toBeTruthy();
+    const responseBody = await newUserResponse.json();
+    expect(responseBody.id).toEqual(101);
+    expect(responseBody.title).toEqual(TITLE);
 
 });
