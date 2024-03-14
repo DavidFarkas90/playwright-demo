39ebc9ae3a89762b98d313e90b1426a311caf1d0
David Farkas <davidfarkas90@gmail.com>
2024-03-14 21:02:58 +0100

    Update file import test

--- e2e/tests/upload-file.spec.ts
+++ e2e/tests/upload-file.spec.ts
@@ -1,19 +1,17 @@
 import { expect, test } from "@playwright/test";
 
-const URL = "https://commitquality.com/practice-file-upload";
+const URL = "https://the-internet.herokuapp.com/upload";
 
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(URL);
+    await page.goto(URL, { timeout: 10000 });
 
 });
 
 test("Upload file", async ({ page }) => {
-    // Start waiting for file chooser before clicking. Note no await.
-    const fileChooserPromise = page.waitForEvent('filechooser');
-    await page.locator("#file-input").click();
-    const fileChooser = await fileChooserPromise;
-    await fileChooser.setFiles("./e2e/downloads/LambdaTest.txt");
-    await page.getByRole('button', { name: /submit/i }).click();
+
+    await page.setInputFiles('input[type="file"]', './e2e/downloads/LambdaTest.txt');
+    await page.click('#file-submit');
+    await expect(page.locator('#uploaded-files')).toContainText('LambdaTest.txt');
 
 });
