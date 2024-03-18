17975f5988927d7e1784c03706b50c47fe916413
David Farkas <davidfarkas90@gmail.com>
2024-03-18 21:31:06 +0100

    Added drag-and-drop test

--- e2e/tests/upload-file.spec.ts
+++ e2e/tests/upload-file.spec.ts
@@ -1,17 +1,31 @@
 import { expect, test } from "@playwright/test";
 
-const URL = "https://the-internet.herokuapp.com/upload";
+test("Upload file heroku", async ({ page }) => {
 
+    const URL = "https://the-internet.herokuapp.com/upload";
 
-test.beforeEach(async ({ page }) => {
     await page.goto(URL, { timeout: 10000 });
-
-});
-
-test("Upload file", async ({ page }) => {
-
     await page.setInputFiles('input[type="file"]', './e2e/downloads/LambdaTest.txt');
     await page.click('#file-submit');
     await expect(page.locator('#uploaded-files')).toContainText('LambdaTest.txt');
+
+});
+
+test("Upload file commitquality", async ({ page }) => {
+
+    const URL_2 = "https://commitquality.com/practice-file-upload";
+
+    await page.goto(URL_2, { timeout: 10000 });
+
+
+    // Start waiting for file chooser before clicking. Note no await.
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.locator('#file-input').click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles('./e2e/downloads/LambdaTest.txt');
+
+    // await page.setInputFiles('#file-input', './e2e/downloads/LambdaTest.txt');
+    // await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.locator('#file-input')).toHaveValue(/LambdaTest.txt/);
 
 });
