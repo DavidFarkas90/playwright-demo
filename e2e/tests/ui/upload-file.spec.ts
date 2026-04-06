417a736c98d203d76555f23c67d249a1c56927de
David Farkas <davidfarkas90@gmail.com>
2026-04-06 18:37:02 +0200

    Deleted obsolete test

--- e2e/tests/ui/upload-file.spec.ts
+++ e2e/tests/ui/upload-file.spec.ts
@@ -13,19 +13,3 @@ test("Upload file heroku", async ({ page }, testInfo) => {
 
   await expect(page.locator("#uploaded-files")).toHaveText("temp.txt");
 });
-
-test("Upload file commit quality", async ({ page }) => {
-  const URL_2 = "https://commitquality.com/practice-file-upload";
-
-  await page.goto(URL_2, { timeout: 10000 });
-
-  // Start waiting for file chooser before clicking. Note no await.
-  const fileChooserPromise = page.waitForEvent("filechooser");
-  await page.locator("#file-input").click();
-  const fileChooser = await fileChooserPromise;
-  await fileChooser.setFiles("./e2e/downloads/LambdaTest.txt");
-
-  // await page.setInputFiles('#file-input', './e2e/downloads/LambdaTest.txt');
-  // await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.locator("#file-input")).toHaveValue(/LambdaTest.txt/);
-});
